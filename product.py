c24e9bf188d08f8acc706f052d9dd2bfaf45ca5d
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-05-11 08:06:28 +0200

    commented product.py

--- product.py
+++ product.py
@@ -1,11 +1,15 @@
+
+# -tc- éliminer les étoiles et trier correctement les import
 from database import *
 from config import *
 from constant import *
 import mysql.connector
 from mysql.connector import errorcode
 
+# -tc- créer également des classe pour catégorie, magasin, favori
 
-class Products:
+
+class Products: # -tc- une classe se nomme au singulier (comme une table d'ailleurs)
 
     def __init__(self, barcode, name, nutriscore, url, market, cat_id):
 
@@ -15,8 +19,9 @@ class Products:
             nutriscore,
             url,
             market,
-            cat_id,
+            cat_id, # -tc- si vous voulez le faire orienté-objet, utiliser un objet pour la catégorie
         ]
+        # -tc- pourquoi un seul attribut et pas des attributs barcode, name, etc.
 
     def add_p(self):
 
